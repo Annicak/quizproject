738e36cdb5a9f4fe62abca2b91986f64b008d34f
Annicaknu <tilfel42@gmail.com>
2016-09-08 12:01:12 +0200

    skapa fyra vyer

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+def startpage(request):
+	return render(request,"quiz/startsida.html")
+def quiz(request):
+	return render(request,"quiz/quizsida.html")
+def question(request):
+	return render(request,"quiz/frågor.html")
+def completed(request):
+	return render(request,"quiz/resultat.html")
+
+
 
 # Create your views here.
