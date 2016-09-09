395bb3cbb0b37e8b51a16d454f3207157b4e822d
Annicaknu <tilfel42@gmail.com>
2016-09-09 10:42:35 +0200

    nytt

--- quiz/models.py
+++ quiz/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 class Quiz(models.Model):
-    quiz_number = models.PositiveIntegerField()
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+	name = models.CharField(max_length=100)
+	def __str__(self):
+		return self.name
 
 class Question(models.Model):
 	question = models.TextField()
@@ -11,6 +11,8 @@ class Question(models.Model):
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
 	quiz = models.ForeignKey(Quiz, related_name="questions")
+
+
 
 
 # Create your models here.
