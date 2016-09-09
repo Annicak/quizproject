79a892f3be16b444070eef1749a9e486b2dd3518
Annicaknu <tilfel42@gmail.com>
2016-09-09 10:29:00 +0200

    nytt

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,7 +1,11 @@
+
+from django.contrib import admin
+from django.conf.urls import include
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [url(r"^$", views.startpage, name="start_page"),
     url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"),
     url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
-    url(r"^quiz/([0-9]+)/completed/$", views.completed, name="result_page"),
+    url(r"^quiz/([0-9]+)/completed/$", views.completed, name="result_page"),url(r'^admin/', include(admin.site.urls)),
+
 ]
